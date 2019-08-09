933f50a57437852a5fad68abe3c36d756dcbc2b4
PcPortatile <cancelli1997@gmail.com>
2019-08-10 00:08:46 +0200

    fixato leak di connessioni al cluster MongoDB

--- mongoutils/mongoclient.py
+++ mongoutils/mongoclient.py
@@ -4,10 +4,11 @@ import pymongo
 class MongoClient:
     """client for Mongo cluster"""
     URI = "mongodb+srv://Api_app:swe2019@clusterdiprova-prsqj.mongodb.net/test?retryWrites=true"
+    instance = pymongo.MongoClient(URI, connect=False)
 
     def __init__(self, db, collection):
         self.db = db
         self.collection = collection
 
     def connect(self):
-        return pymongo.MongoClient(self.URI, connect=False)[self.db][self.collection]
+        return self.instance[self.db][self.collection]
